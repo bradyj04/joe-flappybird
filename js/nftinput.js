b2dec076cb4bbaaf2e0dcecb798280db8e3c1489
bradyj04 <brj842004@gmail.com>
2022-01-13 22:59:56 -0500

    many things; new css; fixes cookie issue; exit but

--- js/nftinput.js
+++ js/nftinput.js
@@ -1,17 +1,27 @@
 function saveInputNFT() {
     /*Get form data and save to variable*/
-    var nftvalue = document.getElementById("nft_url").value;
-    /*Create button when function called*/
-    var playbutton = document.createElement("a");
-    playbutton.innerHTML = "Play Now";
-    playbutton.className = "play-button";
-    playbutton.id = "play-button";
-    playbutton.href = "./game/index.html";
-    document.body.appendChild(playbutton);
-    /*save nftvalue to cookie*/
-    document.cookie = "nft=" + nftvalue;
-    //alert(document.cookie);
-}
-function toFlappy() {
-    window.location='/game/index.html';
+    if(document.getElementById("nft_url").value == "" || document.getElementById("nft_url").value == null) {
+        var field_alert = document.createElement("h2");
+        field_alert.innerHTML = "Please enter a valid NFT URL";
+        field_alert.id = "field_alert";
+        field_alert.classList.add("error-shake");
+        if(document.getElementById("field_alert") == null) {
+            document.body.appendChild(field_alert);
+        }else{
+            document.getElementById("field_alert").remove();
+            document.body.appendChild(field_alert);
+        }
+        
+    }else{
+        var nftvalue = document.getElementById("nft_url").value;
+        /*Create button when function called*/
+        var playbutton = document.createElement("a");
+        playbutton.innerHTML = "Play Now";
+        playbutton.className = "play-button";
+        playbutton.id = "play-button";
+        playbutton.href = "./game/index.html";
+        document.body.appendChild(playbutton);
+        /*save nftvalue to cookie*/
+        document.cookie = "nft=" + nftvalue;
+    }
 }
